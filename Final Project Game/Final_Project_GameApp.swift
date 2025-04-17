d89d3869eff1c047284b2e8359fede121394ac34
NathanKruchko <Kru512490@d204.lths.net>
2025-04-17 11:45:26 -0500

    Initial Commit

---
+++ Final Project Game/Final_Project_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Project_GameApp.swift
+//  Final Project Game
+//
+//  Created by Kruchko, Nathan (512490) on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Final_Project_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
