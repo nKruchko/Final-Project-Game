b88ee8db9d551e48f0af3d7cbd3328595e0b7522
NathanKruchko <Kru512490@d204.lths.net>
2025-04-25 12:06:44 -0500

    Created ground object

--- Final Project Game/GameScene.swift
+++ Final Project Game/GameScene.swift
@@ -9,8 +9,6 @@ import SpriteKit
 import SwiftUI
 
 
-
-
 class GameScene: SKScene, SKPhysicsContactDelegate {
     var test = SKSpriteNode()
     var player = SKSpriteNode()
@@ -35,6 +33,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     
     let groundCategory: UInt32 = 1
+    let objectCategory: UInt32 = 2
     
     override func sceneDidLoad() {
         self.physicsWorld.contactDelegate = self
@@ -43,14 +42,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         ground.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         ground.physicsBody?.node?.name = "ground"
         
-        theGround.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 500, height: 20))
         theGround = SKSpriteNode(color: .orange, size: CGSize(width: 500, height: 20))
-        
-        theGround.physicsBody?.node?.name = "theGround"
         theGround.position = CGPoint(x: size.width / 2, y: 100)
-      //  theGround.physicsBody?.categoryBitMask =
+        theGround.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 500, height: 20))
+        theGround.physicsBody?.categoryBitMask = groundCategory
+        theGround.physicsBody?.collisionBitMask = objectCategory
+        theGround.physicsBody?.node?.name = "theGround"
+        theGround.physicsBody?.affectedByGravity = false
+        theGround.physicsBody?.isDynamic = false
         
-      //  theGround.physicsBody?.affectedByGravity = false
+        
+        
+        
+        
         
         
         addChild(ground)
@@ -61,7 +65,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         character = SKSpriteNode(texture: idleFrames[0])
         character.size = CGSize(width: 50, height: 50)
         character.position = CGPoint(x: size.width/2,y: size.height/2)
+        
+        
         character.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 40, height: 40))
+        character.physicsBody?.collisionBitMask = groundCategory
+        character.physicsBody?.contactTestBitMask = groundCategory
+        character.physicsBody?.categoryBitMask = objectCategory
         
         addChild(character)
         
@@ -111,18 +120,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
 
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
 }
 
